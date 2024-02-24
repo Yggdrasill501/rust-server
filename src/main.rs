61bbd8638dcfc7851b64b25dd14bb5187e950f9b
Filip Žitný <filipzitny@gmail.com>
2024-02-24 22:21:03 +0100

    impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{BufReader,Write};
+
 fn main() {
-    println!("Hello, world!");
-}
+    let listener = std::net::TcpListener::bind("172.0.0.1:9999").unwrap();
+    for mut stream in listener.incoming().flatten(){
+        let mut rdr = std::io::BufReader::new(&mut stream);
+        loop {
+            let mut l = String::new();
+            rdr.read_line(&mut l).unwrap();
+            if l.trim().is_empty(){
+                break;
+            }
+            print!("{l}");
+        }
+        stream.write_all(b"HTTP/1.1 200 OK\r\n\r\nWelcome!").unwrap;
+    }
+        
+} 
